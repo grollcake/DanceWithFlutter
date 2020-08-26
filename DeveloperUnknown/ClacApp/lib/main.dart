afad5d2d32af6e6de9e4b9528e22df519db53ba9
김경민 <_kmn_k@MacBook-Pro.local>
2020-08-26 23:34:47 +0900

    Develop UI

--- DeveloperUnknown/ClacApp/lib/main.dart
+++ DeveloperUnknown/ClacApp/lib/main.dart
@@ -1,117 +1,158 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext c) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Calc App",
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home : MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  String _string = "";
+  bool lastCalc = false;
+  void _clickButton(String context) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if(context == "AC") {
+        _string = "";
+      } else if(context == "/" || context == "*" || context == "+" || context == "-") {
+        if(_string.length == 0 ) {
+          return;
+        } else if(lastCalc) {
+          _string = _string.substring(0, _string.length-1) + context;
+        } else {
+          _string += context;
+          lastCalc = true;
+        }
+      } else if(context == "C") {
+        if(context.length == 0) return;
+        _string = _string.substring(0, _string.length-1);
+      } else {
+        _string += context;
+        lastCalc = false;
+      }
     });
   }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  Widget build(BuildContext c) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Calc App"),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Center(
+                child: Text("$_string", 
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Expanded(
+              flex: 2,
+              child: _createButtonPad(),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+
+  Widget _createButtonPad() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Expanded(
+          flex: 1,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _createButton("AC", 1),
+              _createButton("/", 1),
+              _createButton("*", 1),
+              _createButton("C", 1),
+            ],
+          )
+        ),
+        Expanded(
+          flex: 1,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _createButton("1", 1),
+              _createButton("2", 1),
+              _createButton("3", 1),
+              _createButton("+", 1),
+            ],
+          )
+        ),
+        Expanded(
+          flex: 1,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _createButton("4", 1),
+              _createButton("5", 1),
+              _createButton("6", 1),
+              _createButton("-", 1),
+            ],
+          )
+        ),
+        Expanded(
+          flex: 1,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _createButton("7", 1),
+              _createButton("8", 1),
+              _createButton("9", 1),
+              _createButton(".", 1),
+            ],
+          )
+        ),
+        Expanded(
+          flex: 1,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _createButton(".", 1),
+              _createButton("0", 1),
+              _createButton("00", 1),
+              _createButton("=", 1),
+            ],
+          )
+        ),
+      ],
+    );
+  }
+
+  Widget _createButton(String context, int flex) {
+    return Expanded(
+      flex: flex,
+      child: FlatButton(
+        child: Text("$context",
+          style: TextStyle(
+            fontSize: 20
+          ),
+        ),
+        onPressed: () => _clickButton(context),
+      ),
     );
   }
 }
