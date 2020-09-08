13e2964c051c38276f6e71c0ef154e874431c427
송준수 <jssong80@naver.com>
2020-09-08 23:44:09 +0900

    Practice Dictionary App

--- NewMiracle/Practice/dictionary_app/lib/main.dart
+++ NewMiracle/Practice/dictionary_app/lib/main.dart
@@ -1,21 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart';
+
 import 'dart:async';
 import 'dart:convert';
-
-import 'package:flutter/material.dart';
-import 'package:http/http.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+      title : 'Flutter Demo',
+      theme : ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home : MyHomePage(),       
     );
   }
 }
@@ -26,7 +27,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _url = "https://owlbot.info/api/v4/dictionary/";
+
+  String _url = "https://owlbot.info/api/v4/dictionary";
   String _token = "d50f05de86f946081f8f48dd31af79332a404de8";
 
   TextEditingController _controller = TextEditingController();
@@ -36,63 +38,52 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Timer _debounce;
 
+
+
   _search() async {
-    if (_controller.text == null || _controller.text.length == 0) {
+
+    if(_controller.text == null || _controller.text.length == 0 ){
+
       _streamController.add(null);
-      return;
     }
 
     _streamController.add("waiting");
-    Response response = await get(_url + _controller.text.trim(), headers: {"Authorization": "Token " + _token});
-    _streamController.add(json.decode(response.body));
-  }
 
-  @override
-  void initState() {
-    super.initState();
+    Response res = await get(_url + _controller.text,
+        headers: {"Authorization" : "Token " + _token});
 
-    _streamController = StreamController();
-    _stream = _streamController.stream;
+    _streamController.add(json.decode(res.body));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Flictionary"),
+        title : Text("Flictinary"),
         bottom: PreferredSize(
-          preferredSize: Size.fromHeight(48.0),
+          preferredSize: Size.fromHeight(48),
           child: Row(
-            children: <Widget>[
+            children: [
               Expanded(
                 child: Container(
-                  margin: const EdgeInsets.only(left: 12.0, bottom: 8.0),
+                  margin : EdgeInsets.only(left: 12, bottom : 8),
                   decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(24.0),
+                    color : Colors.white,
+                    borderRadius: BorderRadius.circular(26),
                   ),
-                  child: TextFormField(
-                    onChanged: (String text) {
-                      if (_debounce?.isActive ?? false) _debounce.cancel();
-                      _debounce = Timer(const Duration(milliseconds: 1000), () {
+                  child : TextFormField(
+                    onChanged: (String text){
+                      if(_debounce?.isActive ?? false) _debounce.cancel();
+                      _debounce = Timer(Duration(milliseconds: 1000),(){
                         _search();
                       });
                     },
-                    controller: _controller,
-                    decoration: InputDecoration(
-                      hintText: "Search for a word",
-                      contentPadding: const EdgeInsets.only(left: 24.0),
-                      border: InputBorder.none,
-                    ),
-                  ),
+                  )
                 ),
               ),
               IconButton(
-                icon: Icon(
-                  Icons.search,
-                  color: Colors.white,
-                ),
-                onPressed: () {
+                icon : Icon(Icons.search, color: Colors.white),
+                onPressed: (){
                   _search();
                 },
               )
@@ -100,50 +91,50 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ),
-      body: Container(
-        margin: const EdgeInsets.all(8.0),
-        child: StreamBuilder(
-          stream: _stream,
-          builder: (BuildContext ctx, AsyncSnapshot snapshot) {
-            if (snapshot.data == null) {
+    body : Container(
+      child : StreamBuilder(
+        stream: _stream,
+        builder: (BuildContext context, AsyncSnapshot snapshot){
+
+            if(snapshot.data == null){
               return Center(
-                child: Text("Enter a search word"),
+                child : Text("Enter a search word"),
               );
             }
 
-            if (snapshot.data == "waiting") {
+            if(snapshot.data == "waiting"){
               return Center(
-                child: CircularProgressIndicator(),
+                child : CircularProgressIndicator(),
               );
             }
 
             return ListView.builder(
-              itemCount: snapshot.data["definitions"].length,
-              itemBuilder: (BuildContext context, int index) {
-                return ListBody(
-                  children: <Widget>[
-                    Container(
-                      color: Colors.grey[300],
-                      child: ListTile(
-                        leading: snapshot.data["definitions"][index]["image_url"] == null
-                            ? null
-                            : CircleAvatar(
-                          backgroundImage: NetworkImage(snapshot.data["definitions"][index]["image_url"]),
+                itemCount: snapshot.data["definitions"].length,
+                itemBuilder: (BuildContext context , int index){
+                    return ListBody(
+                      children: [
+                        Container(
+                          color : Colors.grey[300],
+                          child : ListTile(
+                            leading: snapshot.data["definitions"]["image_url"] == null
+                                     ? null
+                                      : CircleAvatar(
+                              backgroundImage: NetworkImage(snapshot.data["definitions"]["image_url"]),
+                            ),
+                            title : Text("("+ snapshot.data["definitions"]["type"] +")")
+                          )
                         ),
-                        title: Text(_controller.text.trim() + "(" + snapshot.data["definitions"][index]["type"] + ")"),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(snapshot.data["definitions"][index]["definition"]),
-                    )
-                  ],
-                );
-              },
-            );
-          },
-        ),
-      ),
+                        Padding(
+                          padding : EdgeInsets.all(8),
+                          child : Text(snapshot.data["definitions"]["definition"])
+                        )
+                      ],
+                    );
+                });
+
+        },
+      )
+    )
     );
   }
 }
