4289ce727b0a286615dbed903108bec2317e067e
grollcake <grollcake@gmail.com>
2021-10-13 12:47:55 +0900

    2번째 암기코딩

---
+++ Era/1013_searchable_list/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:searchable_list/screens/search_user/search_user_screen.dart';
+
+void main() => runApp(MaterialApp(
+      title: 'User search',
+      debugShowCheckedModeBanner: false,
+      home: SearchUserScreen(),
+    ));
