7361ac10741972d7337f6f9bd65b9684c248aa25
grollcake <grollcake@gmail.com>
2021-11-25 23:54:49 +0900

    carousel_slider 패키지를 이용한 연습

---
+++ Era/_1125_carouse_slider/android/app/src/main/kotlin/com/example/carouse_slider/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.carouse_slider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Era/_1125_carouse_slider/android/app/src/main/kotlin/com/example/u_1125_carouse_slider/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.u_1125_carouse_slider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
